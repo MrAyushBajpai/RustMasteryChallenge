9db35275d6a0484eb9bf967a4a59f78890b3b21c
MrAyushBajpai <ayushbajpai1711@gmail.com>
2025-10-08 21:05:09 +0530

    Added Rust Cargo to Basics Folder

---
+++ basics/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+D:\Programming\RustMasteryChallenge\Basics\target\debug\deps\hello_world.d: src\bin\hello_world.rs
+
+D:\Programming\RustMasteryChallenge\Basics\target\debug\deps\hello_world.exe: src\bin\hello_world.rs
+
+src\bin\hello_world.rs:
---
+++ basics/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+D:\Programming\RustMasteryChallenge\Basics\target\debug\hello_world.exe: D:\Programming\RustMasteryChallenge\Basics\src\bin\hello_world.rs
